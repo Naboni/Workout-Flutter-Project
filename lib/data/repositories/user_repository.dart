f92e8281b4a4e80a5427a3e9e0d7b3813c48c2eb
maruf <sherefamaruf@gmail.com>
2021-09-08 17:31:42 +0300

    Added the UI for registration

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:project/data/dataproviders/auth.dart';
 import 'package:project/data/models/user/user.dart';
 
 class UserRepositories {
@@ -38,25 +39,6 @@ class UserRepositories {
   }
 
   Future<Map<String, dynamic>> login(String email, String password) async {
-    print("POST $loginUrl");
-    //! DOESNT CATCH SHIT!!!! DONT TRUST IT;
-    Map<String, String> res;
-    try {
-      var response = await Dio()
-          .post(loginUrl, data: {'email': email, 'password': password});
-      var responseBody = response.data;
-      return {
-        "body": json.encode(responseBody),
-        "status": response.statusCode.toString(),
-        "message": responseBody["message"]
-      };
-    } on DioError catch (e) {
-      return {
-        "status": e.response!.statusCode,
-        "message": e.response!.data["message"]
-      };
-    } catch (e) {
-      return {"status": "69", "message": "Wrong URL or sth"};
-    }
+    return await AuthDataProvider.login(email, password);
   }
 }
